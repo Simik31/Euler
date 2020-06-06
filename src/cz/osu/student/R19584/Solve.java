afca612b47de60033fa41aa8d13e1e62e832162c
Simik31 <40343348+Simik31@users.noreply.github.com>
2020-06-06 19:54:53 +0200

    Add files via upload

---
+++ src/cz/osu/student/R19584/Solve.java
@@ -0,0 +1,17 @@
+package cz.osu.student.R19584;
+
+import cz.osu.student.R19584.Problem_0XX.Solve_0XX;
+
+public class Solve {
+
+    public static void all() {
+        Solve_0XX.all();
+    }
+
+    public static void problem(long id) {
+        if(id < 100) Solve_0XX.problem(id);
+        if(id == 206) Problem_206.main();
+        if(id == 684) Problem_684.main();
+    }
+
+}
