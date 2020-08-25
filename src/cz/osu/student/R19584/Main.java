104c2795a46d171f0432ad58d9d759aed869dce5
Simik31 <40343348+Simik31@users.noreply.github.com>
2020-08-25 21:08:46 +0200

    Add files via upload

---
+++ src/cz/osu/student/R19584/Main.java
@@ -0,0 +1,24 @@
+package cz.osu.student.R19584;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long start_time, problem_id, problems_coded = 75;
+
+        System.out.printf("[*] Coded problems: 1 - %d (%.2f%%)\n", problems_coded, (problems_coded * 100.0 / 723));
+        System.out.print("[*] Which problem do you want to solve (0 = all): ");
+        problem_id = new Scanner(System.in).nextInt();
+
+        start_time = new Date().getTime();
+
+        if(problem_id == 0) Solve.all();
+        else if(problem_id <= problems_coded) Solve.problem(problem_id);
+        else System.out.println("Invalid problem ID " + problem_id);
+
+        System.out.printf("\n[*] DONE IN: %,4dms\n", new Date().getTime() - start_time);
+    }
+}
--- src/cz/osu/student/R19584/Solve.java
+++ src/cz/osu/student/R19584/Solve.java
@@ -1,15 +1,18 @@
 package cz.osu.student.R19584;
 
 import cz.osu.student.R19584.Problem_0XX.Solve_0XX;
+import cz.osu.student.R19584.Problem_2XX.Solve_2XX;
 
 public class Solve {
 
     public static void all() {
         Solve_0XX.all();
+        //Solve_2XX.all();
     }
 
     public static void problem(long id) {
         if(id < 100) Solve_0XX.problem(id);
+        if(id == 206) Solve_2XX.problem(id);
     }
 
 }
